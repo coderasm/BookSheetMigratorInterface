43b24814f9d2eae9c8c498e9dda9a8200a5524b7
'Philip <coderasm84k@gmail.com>
2015-06-16 01:16:00 -0700

    Implemented web sockets

--- BookSheetMigratorInterface/Scripts/main.js
+++ BookSheetMigratorInterface/Scripts/main.js
@@ -1,9 +1,16 @@
 ﻿require.config({
     paths: {
         "jquery": "jquery-1.10.2",
+        "signalr": "jquery.signalR-2.2.0",
+        "signalr.hubs": "/signalr/hubs?",
         "datetimepicker": "bootstrap-datetimepicker",
         "knockout": "knockout-3.3.0",
         "kovalidation": "knockout.validation"
+    },
+    shim: {
+        "jquery": { exports: "$" },
+        "signalr": { deps: ["jquery"] },
+        "signalr.hubs": { deps: ["signalr"] }
     }
 });
 
--- BookSheetMigratorInterface/Startup.cs
+++ BookSheetMigratorInterface/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(BookSheetMigratorInterface.Startup))]
@@ -12,6 +9,7 @@ namespace BookSheetMigratorInterface
     {
         public void Configuration(IAppBuilder app)
         {
+            app.MapSignalR();
             ConfigureAuth(app);
         }
     }
